0cf068e9f250acba9a8bb087d167e4bda37c1138
curry_ina_hurry <trevor.curry@live.com>
2025-09-26 13:41:41 -0400

    initial python code to read contents of book

---
+++ main.py
@@ -0,0 +1,11 @@
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    path_to_file = "books/frankenstein.txt"
+    book_text = get_book_text(path_to_file)
+    print(book_text)
+
+main()
